33073dee5e9b4ce0095eda5de5cc266377cb5f42
Osama Ibhaiss <146546139+osamaibhaiss@users.noreply.github.com>
2024-04-04 14:42:55 -0700

    Add files via upload

---
+++ src/components/About/About.jsx
@@ -0,0 +1,18 @@
+
+import { useState } from 'react';
+
+function About() {
+  const [count, setCount] = useState(0);
+
+  const changeCount = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div>
+      <button onClick={changeCount}>Count: {count}</button>
+    </div>
+  );
+}
+
+export default About;
